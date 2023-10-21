a20ebdeeb9900753fe071d0e572336121092bf55
Viateur <akimanaviateur94@gmail.com>
2023-10-21 21:28:41 +0200

    abstraction

--- src/abstraction/in/jav/Circle.java
+++ src/abstraction/in/jav/Circle.java
@@ -22,3 +22,4 @@ public class Circle extends Shape {
 }
 
 
+
---
+++ src/abstraction/in/jav/Rectangle.java
@@ -0,0 +1,20 @@
+package abstraction.in.jav;
+
+class Rectangle extends Shape {
+    double length;
+    double width;
+
+    public Rectangle(String color, double length, double width)
+    {
+        // calling Shape constructor
+        super(color);
+        this.length = length;
+        this.width = width;
+    }
+    @Override double area() { return length * width; }
+    @Override public String toString()
+    {
+        return "Rectangle color is " + super.getColor()
+                + "and area is : " + area();
+    }
+}
