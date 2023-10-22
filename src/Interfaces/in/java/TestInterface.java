78db443408e80034a8ae5f0b39cddbfb036c6352
Viateur <akimanaviateur94@gmail.com>
2023-10-22 20:31:00 +0200

    interface

---
+++ src/Interfaces/in/java/TestInterface.java
@@ -0,0 +1,13 @@
+package Interfaces.in.java;
+
+public interface TestInterface {
+        // abstract method
+        public void square(int a);
+
+        // default method
+        default void show()
+        {
+            System.out.println("Default Method Executed");
+        }
+
+    }
